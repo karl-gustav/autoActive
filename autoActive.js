60267e957e9ec2d72d6d8741418327f3864450a6
Blake Niemyjski <bniemyjski@gmail.com>
2014-11-20 16:28:33 -0600

    Added the ability to auto-activate any link.
    If the current element contains an href then allow it to be auto activated as well.

--- autoActive.js
+++ autoActive.js
@@ -1,28 +1,43 @@
 (function () {
-    angular.module('autoActive', [])
-        .directive('autoActive', ['$location', function ($location) {
-            return {
-                restrict: 'A',
-                scope: false,
-                link: function (scope, element) {
-                    function setActive() {
-                        var path = $location.path();
-                        if (path) {
-                            angular.forEach(element.find('li'), function (li) {
-                                var anchor = li.querySelector('a');
-                                if (anchor.href.match('#' + path + '(?=\\?|$)')) {
-                                    angular.element(li).addClass('active');
-                                } else {
-                                    angular.element(li).removeClass('active');
-                                }
-                            });
-                        }
-                    }
+  'use strict';
 
-                    setActive();
-
-                    scope.$on('$locationChangeSuccess', setActive);
-                }
+  angular.module('autoActive', [])
+    .directive('autoActive', ['$location', '$timeout', function ($location, $timeout) {
+      return {
+        restrict: 'A',
+        scope: false,
+        link: function (scope, element, attr) {
+          function setActive() {
+            var path = $location.path();
+            if (!path) {
+              return;
             }
-        }]);
+
+            angular.forEach(element.find('li'), function (li) {
+              var anchor = li.querySelector('a');
+              if (anchor.href.match('#' + path + '(?=\\?|$)')) {
+                angular.element(li).addClass('active');
+              } else {
+                angular.element(li).removeClass('active');
+              }
+            });
+
+            if (attr.href) {
+              if (attr.href.match('#' + path + '(?=\\?|$)')) {
+                element.addClass('active');
+              } else {
+                element.removeClass('active');
+              }
+            }
+          }
+
+          var timeout = $timeout(setActive, 1);
+          var unbind = scope.$on('$locationChangeSuccess', setActive);
+          scope.$on('$destroy', function() {
+            $timeout.cancel(timeout);
+            unbind();
+          });
+        }
+      }
+    }]);
 }());
